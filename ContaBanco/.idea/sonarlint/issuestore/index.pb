8129e86e110be03694a83086bd9d91d7d5421e5e
AiltonPireJunior <ailtonsa.junior@outlook.com>
2023-08-22 10:57:11 -0300

    Passando os dados da Conta e recebendo os dados armazenados

---
+++ ContaBanco/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+>
+ContaBanco.iml,1\9\19affee51fdce7f56a2b01cc371c08d9f2a068c2
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+E
+.vscode/settings.json,8\9\89aa447020cdcb580cea0495694d46a55941edbe
+F
+src/ContaTerminal.java,7\a\7ac26ecf1a233eda2e730c4d24c4c5744ca31bf4
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Integer numero;
+        String agencia;
+        String nomeCliente;
+        Float saldo;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por gentileza, digite seu Nome");
+        nomeCliente = scan.next();
+        System.out.println("Por gentileza, digite o Numero da conta");
+        numero = scan.nextInt();
+        System.out.println("Por gentileza, digite o numero da Agencia !");
+        agencia = scan.next();
+        System.out.println("Por gentileza, digite seu Saldo");
+        saldo = scan.nextFloat();
+
+        System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numero, saldo));
+
     }
 }
