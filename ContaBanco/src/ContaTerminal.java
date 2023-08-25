c1747c52a098c634976ebad0330f7bd3c8bdcd4f
AiltonPireJunior <ailtonsa.junior@outlook.com>
2023-08-25 14:37:10 -0300

    adicionando tratativa para Exception do tipo de dado inserido na variável numero e saldo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,8 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int numero;
         String agencia;
@@ -9,17 +10,20 @@ public class ContaTerminal {
         float saldo;
 
         Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("Por gentileza, digite seu Nome");
 
-        System.out.println("Por gentileza, digite seu Nome");
-        nomeCliente = scan.next();
-        System.out.println("Por gentileza, digite o Numero da conta");
-        numero = scan.nextInt();
-        System.out.println("Por gentileza, digite o numero da Agencia !");
-        agencia = scan.next();
-        System.out.println("Por gentileza, digite seu Saldo");
-        saldo = scan.nextFloat();
+            nomeCliente = scan.next();
+            System.out.println("Por gentileza, digite o Numero da conta");
+            numero = scan.nextInt();
+            System.out.println("Por gentileza, digite o numero da Agencia !");
+            agencia = scan.next();
+            System.out.println("Por gentileza, digite seu Saldo");
+            saldo = scan.nextFloat();
 
-        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$%.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
-
+            System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$%.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
+        } catch (InputMismatchException e) {
+            System.out.println("O campo de Conta e saldo devem ser somente números");
+        }
     }
 }
